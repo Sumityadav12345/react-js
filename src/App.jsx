3476d3679bb9c54980d4d2baa4928bc13d839bc6
Sumit <ysumit93310@gmail.com>
2025-07-12 15:14:59 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import Counter from "./Counter";
+import User from "./User";
+import User1 from "./User1";
+import Student from "./Student";
+import Wrapper from "./wrapper";
+
+function App(){
+  const[fruit,setFruit]=useState("Apple");
+  const[display,setDisplay]=useState(true);
+  const handleFruit=()=>{
+    setFruit("banana")
+  }
+  let userObject={
+    name:"sumit yadav",
+    age:22,
+    email:"sumit@gmail.com"
+  }
+  let userObject1={
+    name:"sahil yadav",
+    age:21,
+    email:"sahil@gmail.com"
+  }
+  let userObject2={
+    name:"satish yadav",
+    age:20,
+    email:"satish@gmail.com"
+  }
+
+  const[student,setStudent]=useState("suu")
+  return(
+    <div>
+      <h1>State in react js</h1>
+      <h1>{fruit}</h1>
+      <button onClick={handleFruit}>Change Fruit Name</button>
+      <Counter/>
+      <h1>Tgggle in react js</h1>
+      <button onClick={()=>setDisplay(!display)}>Toggle</button>
+      {/* {
+        display?<h1>Sumit yadav</h1>:null
+      } */}
+      {
+        display?<User/>:null
+      }
+
+      <h1>Props in react js</h1>
+      
+      {/* <User1 name="Sumit yadav" age={22} email="ysumit@gmail.com"/> */}
+      <User1 user={userObject}/>
+      <User1 user={userObject1}/>
+      <User1 user={userObject2}/>
+
+    {student && <Student name={student}/>}
+    <button onClick={()=>setStudent("buuu")}>Update student name</button>
+
+
+  <Wrapper colore="orenge">
+    <h1>hello everone</h1>
+  </Wrapper>
+  <Wrapper color="red">
+    <h1>hello sumit</h1>
+  </Wrapper>
+  <Wrapper color="blue">
+    <h1 >hello sahil</h1>
+  </Wrapper>
+    </div>
+  )
+}
+export default App
---
+++ src/Student.jsx
@@ -0,0 +1,10 @@
+function Student({name}){
+    return(
+        <div>
+            <hr />
+            <h1>{name}</h1>
+            <hr />
+        </div>
+    )
+}
+export default Student;
